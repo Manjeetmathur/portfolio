57a806dbe6b53b9634e68b375af1227caaaf2073
Manjeet Kumar <manjeetkumar62054@gmail.com>
2024-10-21 02:10:12 +0530

    first commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import About from "../pages/About";
 import Home from "../pages/Home";
-import Work from "../pages/Work/work";
+import Work from "../pages/Work/Work";
 import Post from "../pages/Post/Post";
 
 const router = createBrowserRouter([
